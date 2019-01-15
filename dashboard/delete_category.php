cb5dee47e8c02f2458d6f63badf568a4d68f3385
waelvilla <waelvilla@gmail.com>
2019-01-15 10:38:18 +0300

    added delete category

---
+++ dashboard/delete_category.php
@@ -0,0 +1,13 @@
+<?php
+include 'db.php';
+
+$category=$_GET['category'];
+$sql="DELETE FROM categories WHERE category = '$category'";
+$val= $db -> query($sql);
+if($val){
+    echo $category . " deleted successfully";
+    header('location: index.php?active=Categories');
+    die();
+}
+
+?>
--- dashboard/pages/categories.php
+++ dashboard/pages/categories.php
@@ -32,12 +32,50 @@ if(isset($_POST['add_category'])){
 				<h5 class="card-title"><?php echo $row['category']; ?>s</h5>
 				<p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis</p>
 				<div class="container">
-				<a href="#" class="btn btn-primary" >Modify</a>
-				<a href="#" class="btn btn-danger" >Delete</a>
+				<a data-toggle="modal" data-target="#addCategory" class="btn btn-primary" >Modify</a>
+				<a href="delete_category.php?category=<?php echo $row['category'];?>" class="btn btn-danger" >Delete</a>
 				</div>
 			</div>
 		</div>
+		<!-- Edit Category -->
+		<div class="modal fade" id="addCategory" tabindex="-1" role="dialog" aria-labelledby="edit_category_modal" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="edit_category_modal">Edit Category</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<form method="post" enctype="multipart/form-data">
+							<div class="form-group">
+								<label class="col-md-4 control-label" for="category_name">Category Name</label>
+								<div class="col-md-8 ">
+									<input id="category_name" name="category_name" type="text" placeholder="Category Name" class="form-control" value= "<?php echo $row['category']; ?>" required>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-md-4 control-label" for="categ_img">Category Photo</label>
+								<div class="col-md-6">
+								<img src="../img/categories/<?php echo $row['category'];?>.jpg" class="card-img-top img-thumbnail menu-img">
+								</div>
+								<div class="col-md-3 ">
+									<input id="categ_img" name="categ_img" type="file" accept="image/*" required>
+								</div>
+							</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button type="submit" value="submit" class="btn btn-primary" name="add_category">Add Category</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
 	<?php endwhile; ?>
+	
 		<div class="card col-md-3 m-3 view hoverable" style="width: 18rem;">
 			<a data-toggle="modal" data-target="#addCategory">
 				<img src="../img/plus.png" class="card-img-top img-thumbnail menu-img" >
@@ -49,12 +87,12 @@ if(isset($_POST['add_category'])){
 	</div>
 </div>
 
-<!-- Modal -->
-<div class="modal fade" id="addCategory" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Add Category Modal -->
+<div class="modal fade" id="addCategory" tabindex="-1" role="dialog" aria-labelledby="add_category_modal" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add Category</h5>
+        <h5 class="modal-title" id="add_category_modal">Add Category</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -82,3 +120,5 @@ if(isset($_POST['add_category'])){
 		</div>
   </div>
 </div>
+
+
